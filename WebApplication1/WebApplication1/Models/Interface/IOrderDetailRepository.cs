12d4c159e431e2a6aca41471e3e7e627b75195aa
doris <yichungchentw@gmail.com>
2017-07-18 22:37:35 +0800

    Add Repository Interface

---
+++ WebApplication1/WebApplication1/Models/Interface/IOrderDetailRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.Interface
+{
+    public interface IOrderDetailRepository
+    {
+        void Create(Order_Details instance);
+
+        void Update(Order_Details instance);
+
+        void Delete(Order_Details instance);
+
+        Order_Details Get(int productID);
+
+        IQueryable<Order_Details> GetAll();
+
+        void SaveChanges();
+    }
+}
